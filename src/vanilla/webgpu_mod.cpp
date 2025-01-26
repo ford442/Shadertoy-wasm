e02e3fc71662b9c4731fa9461e783147842e237e
Noah Cohn <noahc42@gmail.com>
2025-01-25 22:10:42 -0800

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -239,7 +239,7 @@ passDesc2.timestampWrites=renderTimestampWrites;
 wrpd.at(1,1)=passDesc2;
       
 // if(on_b.at(4,4)==1){
-if (isRenderingPaused==true) {
+if (isRenderingPaused==false) {
 
 INVTextureView=wgpu_texture_create_view(WGPU_Texture.at(0,0,3),&WGPU_TextureViewDescriptor.at(0,0,3));
 wtv.at(6,6)=INVTextureView;
@@ -1216,7 +1216,7 @@ return EM_TRUE;
 extern"C"{
 
 void pauseRendering(bool pause) {
-isRenderingPaused = pause;
+isRenderingPaused = true;
 return;
 }
 
