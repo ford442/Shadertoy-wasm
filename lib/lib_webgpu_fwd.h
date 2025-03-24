d5109e9c6ee43b2925b1e258bc4d3e9906c320f3
Noah Cohn <noahc42@gmail.com>
2025-03-24 16:10:34 -0700

    Update lib_webgpu_fwd.h

--- lib/lib_webgpu_fwd.h
+++ lib/lib_webgpu_fwd.h
@@ -164,19 +164,23 @@ typedef struct WGpuOrigin3D WGpuOrigin3D;
 typedef struct WGpuExtent3D WGpuExtent3D;
 typedef struct WGpuBindGroupLayoutEntry WGpuBindGroupLayoutEntry;
 typedef WGpuObjectBase WGpuImageBitmap;
+typedef struct WGpuPipelineError WGpuPipelineError;
 
 // Callbacks in the order of appearance in the WebIDL:
 
 typedef void (*WGpuRequestAdapterCallback)(WGpuAdapter adapter, void *userData);
 typedef void (*WGpuRequestDeviceCallback)(WGpuDevice device, void *userData);
-typedef void (*WGpuRequestAdapterInfoCallback)(WGpuAdapter adapter, const WGpuAdapterInfo *adapterInfo NOTNULL, void *userData);
-typedef void (*WGpuCreatePipelineCallback)(WGpuDevice device, WGpuPipelineBase pipeline, void *userData);
+typedef void (*WGpuCreatePipelineCallback)(WGpuDevice device, WGpuPipelineError *error, WGpuPipelineBase pipeline, void *userData);
 typedef void (*WGpuBufferMapCallback)(WGpuBuffer buffer, void *userData, WGPU_MAP_MODE_FLAGS mode, double_int53_t offset, double_int53_t size);
 typedef void (*WGpuGetCompilationInfoCallback)(WGpuShaderModule shaderModule, WGpuCompilationInfo *compilationInfo NOTNULL, void *userData);
 typedef void (*WGpuOnSubmittedWorkDoneCallback)(WGpuQueue queue, void *userData);
 typedef void (*WGpuDeviceLostCallback)(WGpuDevice device, WGPU_DEVICE_LOST_REASON deviceLostReason, const char *message, void *userData);
 typedef void (*WGpuDeviceErrorCallback)(WGpuDevice device, WGPU_ERROR_TYPE errorType, const char *errorMessage, void *userData);
 typedef void (*WGpuLoadImageBitmapCallback)(WGpuImageBitmap bitmap, int width, int height, void *userData);
+
+#ifdef __EMSCRIPTEN__
+typedef int OffscreenCanvasId;
+#endif
 
 #ifdef __cplusplus
 } // ~extern "C"
