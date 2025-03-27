a32c1e5f3e97297fabe879d90d4e5e60b0bf937d
Noah Cohn <noahc42@gmail.com>
2025-03-26 18:49:36 -0700

    Update video_jebusb.cpp

--- src/video/video_jebusb.cpp
+++ src/video/video_jebusb.cpp
@@ -461,7 +461,7 @@ contx.disable(gl.DITHER);
 // contx.drawingBufferColorSpace='display-p3';
 
 const g=new GPUX({canvas:bcanvas,context:contx});
-const g2=new GPUX({canvas:scanvas,context:contx2});
+const g2=new GPUX({canvas:scanvas});
 const glslAve=`float Ave(float a,float b,float c){return(a+b+c)/3.0;}`;
 const glslSilver=`float Silver(float a){return((a+0.75+0.75+((a+0.75)/2.0))/4.0);}`;
 const glslGoldR=`float GoldR(float a){return((a+0.831+0.831+0.831+((a+0.831)/2.0))/5.0);}`;
