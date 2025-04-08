5f86f2ee0dfd1b0e607788716c6a5a47d5685477
Noah Cohn <noahc42@gmail.com>
2025-04-07 20:04:21 -0700

    Update pagec_webgpuc.js

--- js/pagec_webgpuc.js
+++ js/pagec_webgpuc.js
@@ -327,10 +327,10 @@ document.getElementById('pmhig').innerHTML=parseInt(window.innerHeight,10);
 document.getElementById('circle').height=parseInt(window.innerHeight,10);
 document.getElementById('circle').width=parseInt(window.innerWidth,10);
 document.getElementById('ldv').src=document.getElementById('isrc').innerHTML;
-  if(media_mode=='img'){
-document.getElementById('mv').height=document.innerHeight;
-document.getElementById('ldv').height=document.innerHeight;
-  }
+//  if(media_mode=='img'){
+//document.getElementById('mv').height=document.innerHeight;
+//document.getElementById('ldv').height=document.innerHeight;
+//  }
   if(media_mode=='vid'){
 document.getElementById('ldv').load();
 document.getElementById('ldv').currentTime=document.getElementById('itim').innerHTML;
