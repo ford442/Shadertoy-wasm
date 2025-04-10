63adb2d07c8b8d61121dbd78d9fdee1359c3f3e1
Noah Cohn <noahc42@gmail.com>
2025-04-10 00:24:36 -0700

    Update video_jebusc2.cpp

--- src/video/video_jebusc2.cpp
+++ src/video/video_jebusc2.cpp
@@ -99,8 +99,8 @@ return;
 // Wrapper function specifically for embind using typed_memory_view
 // This function will be called from JavaScript
 void nanoD_embind(int Fnum, int leng, emscripten::val ptr, emscripten::val aptr) {
-    emscripten::typed_memory_view<double> ptr_view(ptr.as<double*>(), ptr["length"].as<size_t>());
-    emscripten::typed_memory_view<double> aptr_view(aptr.as<double*>(), aptr["length"].as<size_t>());
+    emscripten::typed_memory_view<uint8_t> ptr_view(ptr.as<uint8_t*>(), ptr["length"].as<size_t>());
+    emscripten::typed_memory_view<uint8_t> aptr_view(aptr.as<uint8_t*>(), aptr["length"].as<size_t>());
 
     // Optional: Basic validation of view sizes
     if (ptr_view.size() < leng) {
