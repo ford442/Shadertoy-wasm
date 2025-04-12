4d60d83363eaa3c163b726d8504497cd30064f1c
Noah Cohn <noahc42@gmail.com>
2025-04-12 13:31:10 -0700

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -4,7 +4,7 @@
 
 #include <emscripten/bind.h>
 #include <emscripten/val.h>
-#include "/usr/include/eigen3/Eigen/Dense"
+#include "/usr/include/eigen3/Eigen/Eigen"
 
 namespace fsm = boost::filesystem;
 
