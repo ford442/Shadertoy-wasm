b33a43c2a3abc9749d0cc0daf7de60576a2e3c15
Noah Cohn <noahc42@gmail.com>
2025-04-14 22:16:17 -0700

    Update webgpu_compute_js_mod.cpp

--- src/vanilla/webgpu_compute_js_mod.cpp
+++ src/vanilla/webgpu_compute_js_mod.cpp
@@ -173,7 +173,7 @@ const imageData = image.data;
     const pixelCount = keepSize * keepSize * 4; // RGBA
   for (let i = 0; i < pixelCount; ++i) {
         // Normalize uint8 (0-255) to float (0.0-1.0)
-        frameBufferViewF32[i] = imageData[i] / 255.0;
+        frameBufferViewF32[i] = imageData[i];  //   / 255.0;
     }
 
 // const pixelData = new Float32Array(imageData);
