e1cfecc5b9ea017248ab7f064ccccc19ee400000
Noah Cohn <noahc42@gmail.com>
2025-04-16 09:45:50 -0700

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -24,7 +24,7 @@ void process_copied_data_val(emscripten::val js_typed_array_val) {
 std::vector<float> cpp_copy = emscripten::vecFromJSArray<float>(js_typed_array_val);
 size_t num_elements = (size_t)cpp_copy.size();
 pixel_buffer.resize(num_elements);
-std::transform(cpp_copy.begin(),cpp_copy.end(),pixel_buffer.begin(),[](float val){return val;});
+std::transform(cpp_copy.begin(),cpp_copy.end(),pixel_buffer.begin(),[](float val){return val/255.0f;});
 if(on.at(3,3)==1){
 on_b.at(4,4)=1;
 }
