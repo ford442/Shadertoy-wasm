829989017b1b5b8980e3f41964bc7cbaa9c7ad59
Noah Cohn <noahc42@gmail.com>
2025-04-16 12:41:17 -0700

    Update cropcircle.cpp

--- src/vanilla/cropcircle.cpp
+++ src/vanilla/cropcircle.cpp
@@ -302,15 +302,12 @@ return;
 
 emscripten::val processFloatData(emscripten::val js_float32_array_val) {
     std::vector<float> cpp_vector = emscripten::vecFromJSArray<float>(js_float32_array_val);
-
     if (cpp_vector.size() == 0) {
          return emscripten::val::object();
     }
-
     double sum = 0.0;
     float min_val = std::numeric_limits<float>::max();
     float max_val = std::numeric_limits<float>::lowest();
-
     for(size_t i = 0; i < cpp_vector.size(); ++i) {
         float val = cpp_vector[i];
         sum += val;
@@ -318,14 +315,12 @@ emscripten::val processFloatData(emscripten::val js_float32_array_val) {
         if (val > max_val) max_val = val;
     }
      double avg = sum / cpp_vector.size();
-
     emscripten::val result = emscripten::val::object();
     result.set("average", avg);
     result.set("min", min_val);
     result.set("max", max_val);
     return result;
 }
-
 
 EMSCRIPTEN_BINDINGS(my_module) {
     emscripten::function("processFloatData", &processFloatData);
@@ -464,7 +459,8 @@ let flPB=document.getElementById("flipB");
     tempCtx.drawImage(vd, 0, 0, ww2, h2, dx, dy, ww2, h2);
     const imgData = tempCtx.getImageData(0, 0, squareSize, squareSize);
 
-ctx.drawImage(vd,0,0,ww,h);
+// ctx.drawImage(vd,0,0,ww,h);
+  
 // ctxB.drawImage(vd,0,0,ww,h);
 // ctxC.drawImage(vd,0,0,ww,h);
 // var imgData=ctx.getImageData(0,0,ww,h);
