a680678c088ca89168f5985f1549584b682e31c0
Noah Cohn <noahc42@gmail.com>
2025-04-16 15:12:23 -0700

    Update webgpu_compute_js_mod.cpp

--- src/vanilla/webgpu_compute_js_mod.cpp
+++ src/vanilla/webgpu_compute_js_mod.cpp
@@ -50,22 +50,7 @@ document.getElementById('ldv').load();
 const tem=document.getElementById('tim');
 const ban=document.getElementById('menuBtn');
 const sfr=document.getElementById('slideframe');
-function grab$lt(){$lt=Math.round($lt);}
-$lt=tem.innerHTML;
-grab$lt();
-const slo=new Slideout({'panel':document.getElementById('panel'),'menu':document.getElementById('menu'),'padding':384,'tolerance':70,'easing':'cubic-bezier(.32,2,.55,.27)'});
-ban.addEventListener('click',function(){slo.toggle();sfr.innerHTML='';
-setTimeout(function(){
-grab$lt();slt=$lt/1000;slt=Math.round(slt);
-sfr.innerHTML='<input type='+'"te'+'xt" id'+'="time'+'slider"/'+'>';
-const tsl=new rSlider({target:'#timeslider',values:{min:0.25,max:30.00},
-step:[0.25],labels:false,tooltip:true,scale:false,});
-grab$lt();slt=($lt/1000);slt=slt*100;slt=Math.round(slt);
-slt=slt/100;tsl.setValues(slt);
-document.getElementById('menu').addEventListener('click',function(){
-$ll=tsl.getValue();$ll=$ll*100;$ll=Math.round($ll);$ll=$ll/100;$ll=($ll*1000);tem.innerHTML=$ll;
-});
-setTimeout(function(){slt=tem.innerHTML;},8);},16);});
+
 if(media_mode=='vid'){
 var adr='./intro.mp4';
 wi=1280;
