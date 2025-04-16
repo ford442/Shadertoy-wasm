a28727f97a15bab88e9212e2e77410eee498f690
Noah Cohn <noahc42@gmail.com>
2025-04-15 23:07:16 -0700

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -18,8 +18,8 @@ uintptr_t get_buffer_ptr() {
 return reinterpret_cast<uintptr_t>(pixel_buffer.data());
 }
 */
-float get_buffer_ptr() {
-return reinterpret_cast<float>(pixel_buffer.data());
+float* get_buffer_ptr() {
+return reinterpret_cast<float*>(pixel_buffer.data());
 }
 
 void process_copied_data_val(emscripten::val js_typed_array_val) {
