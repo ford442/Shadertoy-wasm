16de89f313b2aaf8b3bc5f2a02d74a6b089c88b3
Noah Cohn <noahc42@gmail.com>
2025-04-15 21:29:36 -0700

    Update webgpu_compute_js_mod.cpp

--- src/vanilla/webgpu_compute_js_mod.cpp
+++ src/vanilla/webgpu_compute_js_mod.cpp
@@ -481,7 +481,7 @@ console.log('Starting..');
 frameBufferViewF32 = Module.getPixelBufferView();
      //    console.log(`Obtained C++ buffer view with length: ${frameBufferViewF32.length}`);
     const bufferPtr = Module.get_buffer_ptr();
-    const bufferSizeFloats = w$*h$*4*4;
+    const bufferSizeFloats = w$*h$*4;
 const frameView = new Float32Array(Module.HEAPF32.buffer, bufferPtr, bufferSizeFloats);
     console.log(`JS: Created manual view at ${bufferPtr}, size ${bufferSizeFloats}`);
 
