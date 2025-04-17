ef418d539da217c74c4cca577417114d977bb53c
Noah Cohn <noahc42@gmail.com>
2025-04-17 14:13:30 -0700

    Update webgpu_compute_js_mod.cpp

--- src/vanilla/webgpu_compute_js_mod.cpp
+++ src/vanilla/webgpu_compute_js_mod.cpp
@@ -1485,19 +1485,8 @@ if(T){return;}
 for(i=64;i>0;i--){
 var loca=$F+1;if(loca>64){loca=1;}
 var locb=$Bu+1;if(locb>64){locb=1;}
-let frmm=new Float64Array($H,pointc,la);
+var frmm=new Float64Array($H,pointc,la);
 eval("if ($F==="+i+"){var $r"+i+"=t($"+i+"); frmm.set( r($r"+i+") );  var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+");$F="+loca+";$Bu="+locb+";}");
-}
-
-let offset = 0;
-for (let y = 0; y < h$; y++) {
-for (let x = 0; x < h$; x++) {
-const pixel = frmm[y][x]; // GPU.js returns [height][width][channels]
-frameBufferViewF32[offset++] = pixel[0]; // R
-frameBufferViewF32[offset++] = pixel[1]; // G
-frameBufferViewF32[offset++] = pixel[2]; // B
-frameBufferViewF32[offset++] = pixel[3]; // A
-}
 }
 
 var $bb=R(vv);
@@ -1512,6 +1501,16 @@ const aptr_offset = averageDataView.byteOffset;
 // ** Call the bound function using numerical offsets **
 Module.nanoD_unsafe($F, sz, ptr_offset, aptr_offset);
 setTimeout(function(){
+let offset = 0;
+for (let y = 0; y < h$; y++) {
+for (let x = 0; x < h$; x++) {
+// const pixel = frmm[y][x]; // GPU.js returns [height][width][channels]
+frameBufferViewF32[offset++] = frmm[y][x][0]; // R
+frameBufferViewF32[offset++] = frmm[y][x][1]; // G
+frameBufferViewF32[offset++] = frmm[y][x][2]; // B
+frameBufferViewF32[offset++] = frmm[y][x][3]; // A
+}
+}
 M();
 },16.66);
 }
