4463c1d27085bac68deba988c1b5ddee8946656f
Noah Cohn <noahc42@gmail.com>
2025-04-17 07:40:47 -0700

    Update webgpu_compute_js_mod.cpp

--- src/vanilla/webgpu_compute_js_mod.cpp
+++ src/vanilla/webgpu_compute_js_mod.cpp
@@ -206,8 +206,8 @@ function loada(){
 if(lockVid!=1){
 document.querySelector('#ldv').style.height=window.innerHeight+'px';
 document.querySelector('#ldv').style.width='auto';
-document.querySelector('#ldv').style.height=window.innerHeight+'px';
-document.querySelector('#ldv').style.width='auto';
+document.querySelector('#mvi').style.height=window.innerHeight+'px';
+document.querySelector('#mvi').style.width='auto';
 if(media_mode=='vid'){
 mV.addEventListener('canplay',function(){
 // mV.width=this.videoWidth;
