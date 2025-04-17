bc559d1e78a2e98b9af96523dc846c04f030af08
Noah Cohn <noahc42@gmail.com>
2025-04-17 09:56:55 -0700

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -2,6 +2,8 @@
 #include "../../src/vanilla/webgpu_compute_vars_em.cpp"
 #include <boost/filesystem/fstream.hpp>
 
+#include <webgl/webgl2.h>
+
 #include <emscripten/bind.h>
 #include <emscripten/val.h>
 // #include "/usr/include/eigen3/Eigen/Eigen"
