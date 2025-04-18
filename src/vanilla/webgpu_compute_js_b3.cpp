7b6e3189b596b324d16d1b405a2d40026179e635
Noah Cohn <noahc42@gmail.com>
2025-04-18 01:42:32 -0700

    Update webgpu_compute_js_b3.cpp

--- src/vanilla/webgpu_compute_js_b3.cpp
+++ src/vanilla/webgpu_compute_js_b3.cpp
@@ -1488,7 +1488,8 @@ var loca=$F+1;if(loca>64){loca=1;}
 var locb=$Bu+1;if(locb>64){locb=1;}
 eval("if ($F==="+i+"){var $r"+i+"=t($"+i+"); frmm.set( r($r"+i+") );  var $$"+$Bu+"=t(vv);$"+$Bu+".set($$"+$Bu+");$F="+loca+";$Bu="+locb+";}");
 }
-
+console.log(frmm[0][0]);
+    /*
 let offset = 0;
 for (let y = 0; y < h$; y++) {
 for (let x = 0; x < h$; x++) {
@@ -1499,7 +1500,7 @@ frameBufferViewF32[offset++] = pixel[2]; // B
 frameBufferViewF32[offset++] = pixel[3]; // A
 }
 }
-
+*/
 var $bb=R(vv);
 $B.set($bb,0,sz);
 pointb=66*la;
