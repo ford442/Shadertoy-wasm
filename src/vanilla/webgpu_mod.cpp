060931bfa071000a32c3e2324b634ad0bc491bdb
Noah Cohn <noahc42@gmail.com>
2025-04-17 17:24:02 -0700

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -8,7 +8,7 @@
 
 namespace fsm = boost::filesystem;
 
-std::vector<emscripten_align1_float> pixel_buffer;
+boost::container::vector<emscripten_align1_float> pixel_buffer;
 
 emscripten::val getPixelBufferView() {
 return emscripten::val(emscripten::typed_memory_view(pixel_buffer.size(), pixel_buffer.data()));
@@ -18,9 +18,8 @@ uintptr_t get_buffer_ptr() {
 return reinterpret_cast<uintptr_t>(pixel_buffer.data());
 }
 
-
 void process_copied_data_val(emscripten::val js_typed_array_val) {
-std::vector<float> cpp_copy = emscripten::vecFromJSArray<float>(js_typed_array_val);
+std::vector<emscripten_align1_float> cpp_copy = emscripten::vecFromJSArray<emscripten_align1_float>(js_typed_array_val);
 size_t num_elements = (size_t)cpp_copy.size();
 pixel_buffer.resize(num_elements);
 std::transform(cpp_copy.begin(),cpp_copy.end(),pixel_buffer.begin(),[](float val){return val;});
@@ -1225,10 +1224,10 @@ wgpu_adapter_request_device_async(wa.at(0,0),&wdd.at(0,0),ObtainedWebGpuDeviceSt
 }
 
 EM_BOOL WGPU_Start(emscripten_align1_int vsz,emscripten_align1_int sz,emscripten_align1_int sr){
-// size_t num_elements = (size_t)vsz * vsz * 4;
-// pixel_buffer.resize(num_elements);
+size_t num_elements = (size_t)vsz * vsz * 4;
+pixel_buffer.resize(num_elements);
       
-      sze.at(1,1)=sz;
+sze.at(1,1)=sz;
 sze.at(6,6)=sz;
 szeV.at(7,7)=vsz;
 u64_uni.at(4,4)=sr;  //  texture resize amount
@@ -1245,10 +1244,10 @@ return EM_TRUE;
 }
 
 EM_BOOL WGPU_StartC(emscripten_align1_int vsz,emscripten_align1_int sz,emscripten_align1_int sr){
-// size_t num_elements = (size_t)vsz * vsz * 4;
-// pixel_buffer.resize(num_elements);
+size_t num_elements = (size_t)vsz * vsz * 4;
+pixel_buffer.resize(num_elements);
       
-      sze.at(1,1)=sz;
+sze.at(1,1)=sz;
 sze.at(6,6)=sz;
 szeV.at(7,7)=vsz;
 u64_uni.at(4,4)=sr;  //  texture resize amount
