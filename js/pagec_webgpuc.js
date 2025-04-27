6bb96731145f4db58786b1da4fb5a87fc63584fc
Noah Cohn <noahc42@gmail.com>
2025-04-26 17:36:26 -0700

    Update pagec_webgpuc.js

--- js/pagec_webgpuc.js
+++ js/pagec_webgpuc.js
@@ -243,11 +243,12 @@ pnnl.addEventListener('keydown',spKey);
 function loada(){
 if(lockVid!=1){
 // document.getElementById('ldv').height=window.innerHeight;
+  
 if(media_mode=='vid'){
 mV.addEventListener('canplay',function(){
 mV.width=this.videoWidth;
 mV.height=this.videoHeight;
-},{ once: true });
+});
 loadV.addEventListener('canplay',function(){
 loadV.width=this.videoWidth;
 loadV.height=this.videoHeight;
@@ -265,23 +266,23 @@ $pt=Math.round($pt);
 $pt=$pt/1000000;
 document.getElementById('idur').innerHTML=mic/1000000;
 document.getElementById('itim').innerHTML=$pt;
-},{ once: true });
+});
   }
 if(media_mode=='img'){
 mV.addEventListener('load',function(){
 mV.width=this.naturalWidth;
 mV.height=this.naturalHeight;
-},{ once: false });
+});
 loadV.addEventListener('load',function(){
 loadV.width=this.naturalWidth;
 loadV.height=this.naturalHeight;
-document.getElementById('wid').innerHTML=this.naturalWidth;
-document.getElementById('hig').innerHTML=this.naturalHeight;
+document.getElementById('wid').innerHTML=this.width; //this.naturalWidth;
+document.getElementById('hig').innerHTML=this.height; //naturalHeight;
 // document.getElementById('blnnk').innerHTML=Math.max((this.naturalWidth-this.naturalHeight)/2.0,0);
-document.getElementById('blnnk').innerHTML=Math.max((this.naturalWidth-this.naturalHeight)/2.0,0);
+document.getElementById('blnnk').innerHTML=Math.max((this.width-this.height)/2.0,0);
 // document.getElementById('wid').innerHTML=parseInt(window.innerHeight,10);
 //document.getElementById('hig').innerHTML=parseInt(window.innerHeight,10);
-},{ once: true });
+});
   }
 
   
