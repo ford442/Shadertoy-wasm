0bda234655e7b17508fa5cd30d112de64a6ec2e5
Noah Cohn <noahc42@gmail.com>
2025-05-03 17:18:41 -0700

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -48,7 +48,7 @@ bool processFrameAndConvert(emscripten::val uint8_pixel_data_val) {
   
 // emscripten::typed_memory_view<uint8_t> u8_view(uint8_pixel_data_val);
 //  convert_u8_to_float_wasm_simd(u8_view, pixel_buffer);
-convert_u8_to_float_wasm_simd(emscripten::typed_memory_view(pixel_buffer.size(),uint8_pixel_data_val), pixel_buffer);
+convert_u8_to_float_wasm_simd(emscripten::typed_memory_view(pixel_buffer.size(),uint8_pixel_data_val), pixel_buffer.data());
   
 return true;
 }
