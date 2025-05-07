7529a1ddc7ea2f9aeb5f2196fec6c7c9c561ccdb
Noah Cohn <noahc42@gmail.com>
2025-05-06 17:26:32 -0700

    Update cropcircle.cpp

--- src/vanilla/cropcircle.cpp
+++ src/vanilla/cropcircle.cpp
@@ -736,7 +736,7 @@ cnP.setAttribute("style","transform: scaleY(-1.0)");
 // cnPB.setAttribute("style","transform: scaleY(1);");
 }
 function rrra(rta){
-scanvas.setAttribute("style","backgroundColor:'rgba(255,255,255,0.0)';position: absolute;z-index:999993;top:0;left:0;transform: rotate("+rta+"deg)");
+scanvas.setAttribute("style","backgroundColor:'rgba(255,255,255,0.0)';position: absolute;z-index:999994;top:0;left:0;transform: rotate("+rta+"deg)");
 // cnP.setAttribute("style","transform: rotate("+rta+"deg)");
 // cnPB.setAttribute("style","transform:rotate("+rta+"deg);");
 }
@@ -746,7 +746,7 @@ icanvas.setAttribute("style","backgroundColor:'rgba(255,255,255,0.0)';position: 
 // cnPB.setAttribute("style","transform:rotate("+rtb+"deg);");
 }
 function rrrc(rtc) {
-bcanvas.setAttribute("style","backgroundColor:'rgba(255,255,255,0.0)';position: absolute;z-index:999994;top:0;left:0;transform: rotate("+rtc+"deg)");
+bcanvas.setAttribute("style","backgroundColor:'rgba(255,255,255,0.0)';position: absolute;z-index:999993;top:0;left:0;transform: rotate("+rtc+"deg)");
 // cnP.setAttribute("style","transform: rotate("+rtc+"deg)");
 // cnPB.setAttribute("style","transform: rotate("+rtc+"deg);");
 }
