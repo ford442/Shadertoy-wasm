5717b305dadbba4daeba53a78ba294e6ad9af9be
Noah Cohn <noahc42@gmail.com>
2025-05-10 21:05:05 -0700

    Update webgpu_compute_js_mod.cpp

--- src/vanilla/webgpu_compute_js_mod.cpp
+++ src/vanilla/webgpu_compute_js_mod.cpp
@@ -2,8 +2,8 @@ EM_JS(void,js_main,(),{
 
 FS.mkdir('/shader');
 FS.mkdir('/video');
-FS.writeFile('/video/frame.gl','');
-FS.writeFile('/video/frameBFR.gl','');
+// FS.writeFile('/video/frame.gl','');
+// FS.writeFile('/video/frameBFR.gl','');
 let running=0;
 
 let frameBufferViewF32 = []; // The view into C++ memory
