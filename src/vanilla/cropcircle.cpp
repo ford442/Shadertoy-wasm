37053ad3f97d8266f5c083b8f4804b76a8a4ad75
Noah Cohn <noahc42@gmail.com>
2025-05-31 21:12:07 -0700

    Update cropcircle.cpp

--- src/vanilla/cropcircle.cpp
+++ src/vanilla/cropcircle.cpp
@@ -598,19 +598,19 @@ rgbd3[i+3]=255;
  rgbd[i+3]=0;
 }
 }else{           // black / blank
-rgbd[i]=255-(grey+(rgb-128));
-rgbd[i+1]=255-(grey+(rgb-128));
-rgbd[i+2]=255-(grey+(rgb-128));
-rgbd2[i]=255-(grey+(rgb-128));
-rgbd2[i+1]=255-(grey+(rgb-128));
-rgbd2[i+2]=255-(grey+(rgb-128));
-rgbd3[i]=255-(grey+(rgb-128));
-rgbd3[i+1]=255-(grey+(rgb-128));
-rgbd3[i+2]=255-(grey+(rgb-128));
-  
-rgbd[i+3]=((rgb-128)*diff);
-rgbd2[i+3]=((rgb-128)*diff);
-rgbd3[i+3]=((rgb-128)*diff);
+rgbd[i]=(grey+(rgb-128));
+rgbd[i+1]=(grey+(rgb-128));
+rgbd[i+2]=(grey+(rgb-128));
+rgbd2[i]=(grey+(rgb-128));
+rgbd2[i+1]=(grey+(rgb-128));
+rgbd2[i+2]=(grey+(rgb-128));
+rgbd3[i]=(grey+(rgb-128));
+rgbd3[i+1]=(grey+(rgb-128));
+rgbd3[i+2]=(grey+(rgb-128));
+
+rgbd[i+3]=((rgb-125));
+rgbd2[i+3]=((rgb-125);
+rgbd3[i+3]=((rgb-125);
 // rgbd[i+3]=0;
 // rgbd2[i+3]=0;
 // rgbd3[i+3]=0;
