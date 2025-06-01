3631340dfb3400f876d11ce6f73f11f782e2d066
Noah Cohn <noahc42@gmail.com>
2025-05-31 20:35:38 -0700

    Update cropcircle.cpp

--- src/vanilla/cropcircle.cpp
+++ src/vanilla/cropcircle.cpp
@@ -607,10 +607,12 @@ rgbd2[i+2]=255-(grey+(rgb-128));
 rgbd3[i]=255-(grey+(rgb-128));
 rgbd3[i+1]=255-(grey+(rgb-128));
 rgbd3[i+2]=255-(grey+(rgb-128));
-// rgbd[i+3]=((rgb-128)*diff);
-rgbd[i+3]=0;
-rgbd2[i+3]=0;
-rgbd3[i+3]=0;
+rgbd[i+3]=((rgb-128)*diff);
+rgbd2[i+3]=((rgb-128)*diff);
+rgbd3[i+3]=((rgb-128)*diff);
+// rgbd[i+3]=0;
+// rgbd2[i+3]=0;
+// rgbd3[i+3]=0;
 }
 }
 
