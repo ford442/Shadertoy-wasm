6e76cc63f259bc7ce5d87df4fb7e74be5d67b7b5
Noah Cohn <noahc42@gmail.com>
2025-06-02 04:17:39 -0700

    Update wasm_loader_pm.cpp

--- src/vanilla/wasm_loader_pm.cpp
+++ src/vanilla/wasm_loader_pm.cpp
@@ -186,13 +186,13 @@ xhr.send();
 */
 
   //  1ijs
-  
+let scrsrc=querySelector('#PMmodulePath').innerHTML;
 let scr=document.createElement("script");
 scr.async=true;
 scr.charset='utf-8';
 scr.type='text/javascript';
 scr.defer=true;
-scr.src="https://noahcohn.com/projectm-v.04-thread.1ijs";
+scr.src=scrsrc; // "https://noahcohn.com/projectm-v.04-thread.1ijs";
 document.body.appendChild(scr);
 setTimeout(function(){
 var Module=createModule();
