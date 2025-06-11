7443eebce0f9c36c42039a1f8580d84195635fa5
Noah Cohn <noahc42@gmail.com>
2025-06-11 04:33:15 -0700

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -1512,7 +1512,7 @@ navigator_gpu_request_adapter_async(&wao.at(0,0),ObtainedWebGpuAdapterStart,0);
 return EM_TRUE;
 }
 
-#include "../../src/vanilla/webgpu_compute_js_mod.cpp"
+#include "../../src/vanilla/webgpu_compute_js_handle.cpp"
 
 extern"C"{
 
