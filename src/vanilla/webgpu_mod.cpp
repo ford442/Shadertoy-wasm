6f506fa87a58dbe4e84cac1052a0a932da1a2acd
Noah Cohn <noahc42@gmail.com>
2025-07-31 22:53:20 -0700

    Update webgpu_mod.cpp

--- src/vanilla/webgpu_mod.cpp
+++ src/vanilla/webgpu_mod.cpp
@@ -575,7 +575,7 @@ const char * comp_body = rd_fl(FnmC);
 canvasFormat=navigator_gpu_get_preferred_canvas_format();
 wtf.at(0,0)=WGPU_TEXTURE_FORMAT_RGBA16FLOAT;
 
-WGPUTextureFormat viewFormats[] = { WGPU_TEXTURE_FORMAT_RGBA16FLOAT };
+WGPUTextureFormat viewFormats[0] = { WGPU_TEXTURE_FORMAT_RGBA16FLOAT };
 
 wtf.at(2,2)=WGPU_TEXTURE_FORMAT_RGBA32FLOAT;
 // wtf.at(0,0)=navigator_gpu_get_preferred_canvas_format();
