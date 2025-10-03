f16ad56a5b0a6ce9dbb5cf25f93f7682264cad6f
Noah Cohn <noahc42@gmail.com>
2025-10-03 08:18:17 -0700

    Update video_jebusc3.cpp

--- src/video/video_jebusc3.cpp
+++ src/video/video_jebusc3.cpp
@@ -261,12 +261,12 @@ agav.fill(min,100,33);
 agav.fill(max,200,33);
 const bcanvas=document.querySelector("#bcanvas");
 const contx=bcanvas.getContext("webgl2",{
-colorType:'float64',
+// colorType:'float32',
 precision:'highp',
 colorSpace:'display-p3',
 alpha:true,
-depth:false,
-stencil:false,
+depth:true,
+stencil:true,
 preserveDrawingBuffer:false,
 premultipliedAlpha:false,
 desynchronized:false,
@@ -1056,8 +1056,8 @@ EGL_NONE
   
 emscripten_webgl_init_context_attributes(&attr);
 attr.alpha=EM_TRUE;
-attr.stencil=EM_FALSE;
-attr.depth=EM_FALSE;
+attr.stencil=EM_TRUE;
+attr.depth=EM_TRUE;
 attr.antialias=EM_TRUE;
 attr.premultipliedAlpha=EM_FALSE;
 attr.preserveDrawingBuffer=EM_FALSE;
