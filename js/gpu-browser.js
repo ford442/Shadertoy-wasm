e5fea3f5600d3264f2e18311e1e098e97272fc3e
Noah Cohn <noahc42@gmail.com>
2025-10-03 08:24:21 -0700

    Update gpu-browser.js

--- js/gpu-browser.js
+++ js/gpu-browser.js
@@ -18378,6 +18378,7 @@ class WebGL2Kernel extends WebGLKernel {
     const settings = {
       alpha: true,
       depth: false,
+      powerPreference:'high-performance',
       antialias: true
     };
     return this.canvas.getContext('webgl2', settings);
