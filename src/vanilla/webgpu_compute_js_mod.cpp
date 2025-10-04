2628d53dcc616940ad31e297bbce1d6b400d4eee
Noah Cohn <noahc42@gmail.com>
2025-10-04 01:06:04 -0700

    Update webgpu_compute_js_mod.cpp

--- src/vanilla/webgpu_compute_js_mod.cpp
+++ src/vanilla/webgpu_compute_js_mod.cpp
@@ -540,7 +540,7 @@ OffscCnv.width=keepSize;
 bcnv.width=keepSize;
 bcnv.style.width=keepSize+'px';
 const gl3=OffscCnv.getContext('2d',{
-// colorType:'float32',
+colorType:'float16',
 alpha:true,
 willReadFrequently:true,
 stencil:false,
