1331e507733c0d6e26343ae348c9d845f69522cb
Noah Cohn <noahc42@gmail.com>
2025-11-13 08:18:46 -0800

    update webgpu

--- lib/lib_webgpu_cpp11.cpp
+++ lib/lib_webgpu_cpp11.cpp
@@ -31,6 +31,7 @@ const WGpuTextureDescriptor WGPU_TEXTURE_DESCRIPTOR_DEFAULT_INITIALIZER = {
   WGPU_TEXTURE_DIMENSION_2D, /* dimension */
   0, /* format */
   0, /* usage */
+  0, /* unused_padding */
 };
 
 const WGpuTextureViewDescriptor WGPU_TEXTURE_VIEW_DESCRIPTOR_DEFAULT_INITIALIZER = {
@@ -42,6 +43,7 @@ const WGpuTextureViewDescriptor WGPU_TEXTURE_VIEW_DESCRIPTOR_DEFAULT_INITIALIZER
   0, /* mipLevelCount */
   0, /* baseArrayLayer */
   0, /* arrayLayerCount */
+  "rgba" /* swizzle */
 };
 
 const WGpuExternalTextureDescriptor WGPU_EXTERNAL_TEXTURE_DESCRIPTOR_DEFAULT_INITIALIZER = {
@@ -90,7 +92,7 @@ const WGpuShaderModuleCompilationHint WGPU_SHADER_MODULE_COMPILATION_HINT_DEFAUL
 const WGpuCommandEncoderDescriptor WGPU_COMMAND_ENCODER_DESCRIPTOR_DEFAULT_INITIALIZER = {
 };
 
-const WGpuImageCopyBuffer WGPU_IMAGE_COPY_BUFFER_DEFAULT_INITIALIZER = {};
+const WGpuTexelCopyBufferInfo WGPU_TEXEL_COPY_BUFFER_INFO_DEFAULT_INITIALIZER = {};
 
 const WGpuComputePassTimestampWrites WGPU_COMPUTE_PASS_TIMESTAMP_WRITES_DEFAULT_INITIALIZER = {
   0, /* querySet */
@@ -132,9 +134,11 @@ const WGpuCanvasConfiguration WGPU_CANVAS_CONFIGURATION_DEFAULT_INITIALIZER = {
   WGPU_TEXTURE_USAGE_RENDER_ATTACHMENT, /* usage */
   0, /* numViewFormats */
   nullptr, /* viewFormats */
+  PTR_PAD_ZERO
   HTML_PREDEFINED_COLOR_SPACE_SRGB, /* colorSpace */
   WGPU_CANVAS_TONE_MAPPING_DEFAULT_INITIALIZER, /* toneMapping */
   WGPU_CANVAS_ALPHA_MODE_OPAQUE, /* alphaMode */
+  0 /* unused_padding */
 };
 
 const WGpuRenderPassTimestampWrites WGPU_RENDER_PASS_TIMESTAMP_WRITES_DEFAULT_INITIALIZER = {
@@ -144,13 +148,13 @@ const WGpuRenderPassTimestampWrites WGPU_RENDER_PASS_TIMESTAMP_WRITES_DEFAULT_IN
 };
 
 const WGpuRenderPassDescriptor WGPU_RENDER_PASS_DESCRIPTOR_DEFAULT_INITIALIZER = {
-  0, /* numColorAttachments */
+  0, /* maxDrawCount */
   0, /* colorAttachments */
+  PTR_PAD_ZERO /* unused_padding */
+  0, /* numColorAttachments */
   WGPU_RENDER_PASS_DEPTH_STENCIL_ATTACHMENT_DEFAULT_INITIALIZER, /* depthStencilAttachment */
   0, /* occlusionQuerySet */
-  0, /* maxDrawCount */
   WGPU_RENDER_PASS_TIMESTAMP_WRITES_DEFAULT_INITIALIZER, /* timestampWrites */
-  0 /* unusedPadding */
 };
 
 const WGpuColorTargetState WGPU_COLOR_TARGET_STATE_DEFAULT_INITIALIZER = {
@@ -172,23 +176,27 @@ const WGpuColorTargetState WGPU_COLOR_TARGET_STATE_DEFAULT_INITIALIZER = {
 
 const WGpuRenderPipelineDescriptor WGPU_RENDER_PIPELINE_DESCRIPTOR_DEFAULT_INITIALIZER = {
   { /* vertex */
-    0, /* module */
     nullptr, /* entryPoint */
-    0, /* numBuffers */
+    PTR_PAD_ZERO
     nullptr, /* buffers */
-    0, /* numConstants */
+    PTR_PAD_ZERO
     nullptr, /* constants */
+    PTR_PAD_ZERO
+    0, /* module */
+    0, /* numBuffers */
+    0, /* numConstants */
+    0, /* unused_padding */
   },
   { /* primitive */
     WGPU_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST, /* topology */
     0, /* stripIndexFormat */
     WGPU_FRONT_FACE_CCW, /* frontFace */
     WGPU_CULL_MODE_NONE, /* cullMode */
-    false /* unclippedDepth */
+    WGPU_FALSE /* unclippedDepth */
   },
   { /* depthStencil */
     0, /* format */
-    false, /* depthWriteEnabled */
+    WGPU_FALSE, /* depthWriteEnabled */
     WGPU_COMPARE_FUNCTION_INVALID, /* depthCompare */
     0xFFFFFFFFu, /* stencilReadMask */
     0xFFFFFFFFu, /* stencilWriteMask */
@@ -214,15 +222,21 @@ const WGpuRenderPipelineDescriptor WGPU_RENDER_PIPELINE_DESCRIPTOR_DEFAULT_INITI
     0xFFFFFFFFu, /* mask */
     false /* alphaToCoverageEnabled */
   },
+  0, /* unused_padding */
   { /* fragment */
-    0, /* module */
     nullptr, /* entryPoint */
-    0, /* numTargets */
+    PTR_PAD_ZERO
     nullptr, /* targets */
+    PTR_PAD_ZERO
+    nullptr, /* constants */
+    PTR_PAD_ZERO
+    0, /* module */
+    0, /* numTargets */
     0, /* numConstants */
-    nullptr /* constants */
+    0, /* unused_padding */
   },
-  0 /* layout */
+  0, /* layout */
+  0, /* unused_padding2 */
 };
 
 extern const WGpuExtent3D WGPU_EXTENT_3D_DEFAULT_INITIALIZER = {
@@ -246,7 +260,7 @@ extern const WGpuRenderPassColorAttachment WGPU_RENDER_PASS_COLOR_ATTACHMENT_DEF
   }
 };
 
-extern const WGpuImageCopyExternalImage WGPU_IMAGE_COPY_EXTERNAL_IMAGE_DEFAULT_INITIALIZER = {
+extern const WGpuCopyExternalImageSourceInfo WGPU_COPY_EXTERNAL_IMAGE_SOURCE_INFO_DEFAULT_INITIALIZER = {
   0, /* source */
   { /* origin */
       0,
@@ -255,7 +269,7 @@ extern const WGpuImageCopyExternalImage WGPU_IMAGE_COPY_EXTERNAL_IMAGE_DEFAULT_I
   false /* flipY */
 };
 
-extern const WGpuImageCopyTexture WGPU_IMAGE_COPY_TEXTURE_DEFAULT_INITIALIZER = {
+extern const WGpuTexelCopyTextureInfo WGPU_TEXEL_COPY_TEXTURE_INFO_DEFAULT_INITIALIZER = {
   0, /* texture */
   0, /* mipLevel */
   { /* origin */
@@ -266,7 +280,7 @@ extern const WGpuImageCopyTexture WGPU_IMAGE_COPY_TEXTURE_DEFAULT_INITIALIZER = 
   WGPU_TEXTURE_ASPECT_ALL /* aspect */
 };
 
-extern const WGpuImageCopyTextureTagged WGPU_IMAGE_COPY_TEXTURE_TAGGED_DEFAULT_INITIALIZER = {
+extern const WGpuCopyExternalImageDestInfo WGPU_COPY_EXTERNAL_IMAGE_DEST_INFO_DEFAULT_INITIALIZER = {
   0, /* texture */
   0, /* mipLevel */
   { /* origin */
